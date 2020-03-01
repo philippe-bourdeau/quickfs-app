c23d0ec136f6cba039f1a2b5bc1f4a635b5d7302
pbb <bourdeau.phil@gmail.com>
2020-02-29 20:24:22 -0500

    boilerplate for sending requests to quickfs API

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,44 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Support\Facades\Route;
 
-Route::get('test', function () {
-    return 'boo';
+
+Route::get('raw/{ticker}', function (\Illuminate\Http\Request $request) {
+    $companyName = $request->route('ticker');
+
+    $requestBody = \GuzzleHttp\json_encode([
+        'data' => [
+            $companyName =>
+                [
+                    'revenue' => sprintf('QFS(%s,revenue,FY-19:FY)', $companyName)
+                ]
+        ]
+    ]);
+
+    $request = new Request(
+        'POST',
+        '/v1/data/batch',
+        [
+            config('quickfs.auth-header') => config('quickfs.api-key'),
+            'Content-Type' => 'application/json',
+        ],
+        $requestBody
+    );
+
+    $client = new Client([
+        'base_uri' => config('quickfs.base-uri'),
+    ]);
+
+    try {
+        $response = $client->send($request);
+
+        return $response->getBody()->getContents();
+    } catch (RequestException $exception) {
+        return $exception->getResponse();
+    }
+
 });
