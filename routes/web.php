c965f81e3d2f4c528db2536d95bacb167a45780b
pbb <bourdeau.phil@gmail.com>
2020-03-05 22:26:33 -0500

    Base object : Financial Statement

--- routes/web.php
+++ routes/web.php
@@ -18,32 +18,38 @@ use Illuminate\Support\Facades\Route;
 
 /**
  * {
-"T:CA" : {
-"market_capitalization" : 10,
-"outstanding_share": 10,
-"revenue": [],
-"operating_cash_flow": [],
-"capital_expenditure": [],
-"earnings" : []
-}
-}
+ * "T:CA" : {
+ * "market_capitalization" : 10,
+ * "outstanding_shares": 10,
+ * "revenue": [],
+ * "operating_cash_flow": [],
+ * "capital_expenditure": [],
+ * "net_income" : []
+ * }
+ * }
  */
 
-Route::get('raw/{ticker}', function (\Illuminate\Http\Request $request) {
-    $companyName = $request->route('ticker');
+//const NET_INCOME = 'net_income';
+//const REVENUE = 'revenue';
 
-    $requestBody = \GuzzleHttp\json_encode([
-//        'data' => [
-//            $companyName =>
-//                [
-//                    'revenue' => sprintf('QFS(%s,revenue,FY-19:FY)', $companyName)
-//                ]
-//        ]
-    ]);
+Route::get('raw/{ticker}', function (\Illuminate\Http\Request $request) {
+    $ticker = $request->route('ticker');
+
+    $requestBody = \GuzzleHttp\json_encode(
+        [
+            'data' => [
+                'period_end_date' => sprintf('QFS(%s,period_end_date,FY-9:FY)', $ticker),
+                'revenue' => sprintf('QFS(%s,revenue,FY-9:FY)', $ticker),
+                'operating_cash_flow' => sprintf('QFS(%s,cf_cfo,FY-9:FY)', $ticker),
+                'capex' => sprintf('QFS(%s,capex,FY-9:FY)', $ticker),
+                'net_income' => sprintf('QFS(%s,net_income,FY-9:FY)', $ticker),
+            ]
+        ]
+    );
 
     $request = new Request(
-        'GET',
-        '/v1/metrics',
+        'POST',
+        '/v1/data/batch',
         [
             config('quickfs.auth-header') => config('quickfs.api-key'),
             'Content-Type' => 'application/json',
@@ -57,11 +63,32 @@ Route::get('raw/{ticker}', function (\Illuminate\Http\Request $request) {
 
     try {
         $response = $client->send($request);
-
-        dd($response->getBody()->getContents());
-        return $response->getBody()->getContents();
+        $contents = $response->getBody()->getContents();
     } catch (RequestException $exception) {
         return $exception->getResponse();
     }
+
+    $object = \GuzzleHttp\json_decode($contents);
+    $data = $object->data;
+
+    $statements = collect();
+    for ($i= 0; $i<10;$i++ ) {
+        $container = [
+            'ticker' => $ticker,
+            'year' => array_pop($data->period_end_date),
+            'cash_flow_statement' => [
+                'revenue' => array_pop($data->revenue),
+                'operating_cash_flow' => array_pop($data->operating_cash_flow),
+                'capex' => array_pop($data->capex)
+            ],
+            'income_statement' => [
+                'net_income' => array_pop($data->net_income)
+            ]
+        ];
+
+        $statements->prepend($container);
+    }
+
+    return \GuzzleHttp\json_encode($statements->all());
 
 });
